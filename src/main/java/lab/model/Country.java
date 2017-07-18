bcc9965c164a021db1c6d3d6f2951edac43d6b84
Ilia Gusevskii <il.gusevskiye@gmail.com>
2017-07-18 17:12:00 +0300

    initial commit

---
+++ src/main/java/lab/model/Country.java
@@ -0,0 +1,11 @@
+package lab.model;
+
+import java.io.Serializable;
+
+public interface Country {
+    int getId();
+
+    String getName();
+
+    String getCodeName();
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,44 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import lab.model.Person;
+import lab.model.SimpleCountry;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import lab.model.UsualPerson;
+
+public class HelloWorldTest {
+
+    protected static final String APPLICATION_CONTEXT_XML_FILE_NAME = "src/test/resources/application-context.xml";
+
+    private Person expectedPerson;
+
+    private BeanFactory context;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        context = new FileSystemXmlApplicationContext(
+                new String[]{APPLICATION_CONTEXT_XML_FILE_NAME});
+        expectedPerson = getExpectedPerson();
+    }
+
+    @Test
+    void testInitPerson() {
+        Person person = context.getBean("person", Person.class);
+        assertEquals(expectedPerson, person);
+//		System.out.println(person);
+    }
+
+    private Person getExpectedPerson() {
+        return new UsualPerson()
+                .setAge(35)
+                .setName("John Smith")
+                .setCountry(new SimpleCountry()
+                        .setId(1)
+                        .setName("Russia")
+                        .setCodeName("RU")
+                );
+    }
+}
