b487926a7357ff52df3ef0eac1da1c9412033486
Ivan Chuvakhin <ivan_chuvakhin@epam.com>
2017-07-19 11:59:51 +0300

    Egor Zakovriashin and Ivan Chuvakhin

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -6,36 +6,39 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
 
-	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "src/test/resources/application-context.xml";
+    private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "src/test/resources/application-context.xml";
 
-	private UsualPerson expectedPerson;
+    private UsualPerson expectedPerson;
 
-	private BeanFactory context;
+    private BeanFactory context;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		context = new FileSystemXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
-		expectedPerson = getExpectedPerson();
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+        context = new FileSystemXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+        expectedPerson = getExpectedPerson();
+    }
 
-	@Test
-	void testInitPerson() {
-		Person person = context.getBean("person", Person.class);
-		assertEquals(expectedPerson, person);
+    @Test
+    void testInitPerson() {
+        Person person = context.getBean("person", Person.class);
+        assertEquals(expectedPerson, person);
 //		System.out.println(person);
-	}
+    }
 
-	private UsualPerson getExpectedPerson() {
-		return new UsualPerson()
-				.setAge(35)
-				.setName("John Smith")
-				.setCountry(new SimpleCountry()
-						.setId(1)
-						.setName("Russia")
-						.setCodeName("RU"));
-	}
+    private UsualPerson getExpectedPerson() {
+        return new UsualPerson()
+                .setAge(35)
+                .setName("John Smith")
+                .setCountry(new SimpleCountry()
+                        .setId(1)
+                        .setName("Russia")
+                        .setCodeName("RU"))
+                        .setContacts(Arrays.asList("one", "two", "three"));
+    }
 }
