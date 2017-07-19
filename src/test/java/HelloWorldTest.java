dd62a0ab0a12b1678f933ce723925d6ba68bca75
Ekaterina <kate.son2015@yandex.ru>
2017-07-19 12:46:05 +0300

    Green tests

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,11 @@
+import lab.model.Person;
 import lab.model.SimpleCountry;
 import lab.model.UsualPerson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
