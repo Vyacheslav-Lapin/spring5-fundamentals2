bfb2c360065d532c34c25f04388e855443e6c7f8
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-07-19 16:16:38 +0300

    green tests

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -10,32 +10,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
 
-	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "src/test/resources/application-context.xml";
+    private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "src/test/resources/application-context.xml";
 
-	private UsualPerson expectedPerson;
+    private UsualPerson expectedPerson;
 
-	private BeanFactory context;
+    private BeanFactory context;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		context = new FileSystemXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
-		expectedPerson = getExpectedPerson();
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+        context = new FileSystemXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+        expectedPerson = getExpectedPerson();
+    }
 
-	@Test
-	void testInitPerson() {
-		Person person = context.getBean("person", Person.class);
-		assertEquals(expectedPerson, person);
+    @Test
+    void testInitPerson() {
+        Person person = context.getBean("person", Person.class);
+        assertEquals(expectedPerson, person);
 //		System.out.println(person);
-	}
+    }
 
-	private UsualPerson getExpectedPerson() {
-		return new UsualPerson()
-				.setAge(35)
-				.setName("John Smith")
-				.setCountry(new SimpleCountry()
-						.setId(1)
-						.setName("Russia")
-						.setCodeName("RU"));
-	}
+    private UsualPerson getExpectedPerson() {
+        return new UsualPerson()
+                .setAge(35)
+                .setName("John Smith")
+                .setCountry(new SimpleCountry()
+                        .setId(1)
+                        .setName("Russia")
+                        .setCodeName("RU"));
+    }
 }
