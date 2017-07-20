d2d825750077036bd1450740ac1feeca1ca782de
Evgenii Sidorenko <Evgenii_Sidorenko@epam.com>
2017-07-20 16:21:55 +0300

    test for Country class

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,5 @@
+import lab.model.Country;
+import lab.model.Person;
 import lab.model.SimpleCountry;
 import lab.model.UsualPerson;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,31 +11,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
 
-	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
+    private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
 
-	private UsualPerson expectedPerson;
+    private Person expectedPerson;
+    private Country expectedCountry;
 
-	private BeanFactory context;
+    private BeanFactory context;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
-		expectedPerson = getExpectedPerson();
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+        context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+        expectedPerson = getExpectedPerson();
+        expectedCountry = getExpectedCountry();
+    }
 
-	@Test
-	void testInitPerson() {
-		assertEquals(expectedPerson, context.getBean("person"));
-//		System.out.println(person);
-	}
+    @Test
+    void testInitPerson() {
+        assertEquals(expectedPerson, context.getBean("person"));
+    }
 
-	private UsualPerson getExpectedPerson() {
-		return new UsualPerson()
-				.setAge(35)
-				.setName("John Smith")
-				.setCountry(new SimpleCountry()
-						.setId(1)
-						.setName("Russia")
-						.setCodeName("RU"));
-	}
+    @Test
+    void testInitCountry() {
+        assertEquals(expectedCountry, context.getBean("country"));
+    }
+
+    private Person getExpectedPerson() {
+        return new UsualPerson()
+                .setAge(35)
+                .setName("John Smith")
+                .setCountry(new SimpleCountry()
+                        .setId(1)
+                        .setName("Russia")
+                        .setCodeName("RU"));
+    }
+
+    private Country getExpectedCountry() {
+        return new SimpleCountry()
+                .setId(1)
+                .setName("Russia")
+                .setCodeName("RU");
+    }
 }
