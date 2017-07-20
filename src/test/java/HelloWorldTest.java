b54c9d37c1f5d81361e31552a2371ff8071099b5
Kirill Volkov <neborodulin@gmail.com>
2017-07-20 10:02:11 +0300

    test added

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,3 +1,4 @@
+import lab.model.Country;
 import lab.model.Person;
 import lab.model.SimpleCountry;
 import lab.model.UsualPerson;
@@ -8,11 +9,13 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class HelloWorldTest {
+
+public class HelloWorldTest {
 
 	private static final String APPLICATION_CONTEXT_XML_FILE_NAME = "src/test/resources/application-context.xml";
 
 	private UsualPerson expectedPerson;
+	private SimpleCountry expectedCountry;
 
 	private BeanFactory context;
 
@@ -20,14 +23,23 @@ class HelloWorldTest {
 	void setUp() throws Exception {
 		context = new FileSystemXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
 		expectedPerson = getExpectedPerson();
+		expectedCountry = getExpectedCountry();
 	}
 
 	@Test
 	void testInitPerson() {
 		Person person = context.getBean("person", Person.class);
 		assertEquals(expectedPerson, person);
-//		System.out.println(person);
+		System.out.println(person);
 	}
+
+	@Test
+	void testInitCountry() {
+		Country country = context.getBean("simpleCountry2", Country.class);
+		assertEquals(expectedCountry, country);
+	}
+
+
 
 	private UsualPerson getExpectedPerson() {
 		return new UsualPerson()
@@ -37,5 +49,11 @@ class HelloWorldTest {
 						.setId(1)
 						.setName("Russia")
 						.setCodeName("RU"));
+	}
+
+	private SimpleCountry getExpectedCountry() {
+		return new SimpleCountry()
+				.setId(2)
+				.setName("Israel");
 	}
 }
