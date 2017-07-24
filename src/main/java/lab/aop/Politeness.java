ece30f179bb8aa6db4d956f0247e3a5342dcd227
Denis Verkhoturov <mod.satyr@gmail.com>
2017-07-24 11:10:36 +0300

    feat: lab5 task

--- src/main/java/lab/aop/Politeness.java
+++ src/main/java/lab/aop/Politeness.java
@@ -3,37 +3,37 @@ package lab.aop;
 import lab.model.Person;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class Politeness {
 
     @Before("execution(* sellSquishee(..))")
-    public void sayHello(JoinPoint joinPiont) {
-        AopLog.append("Hello " + ((Person) joinPiont.getArgs()[0]).getName() + ". How are you doing? \n");
+    public void sayHello(JoinPoint joinPoint) {
+        AopLog.append(String.format("Hello %s. How are you doing?%n", ((Person) joinPoint.getArgs()[0]).getName()));
     }
 
     @AfterReturning(pointcut = "execution(* sellSquishee(..))",
             returning = "retVal", argNames = "retVal")
     public void askOpinion(Object retVal) {
-        AopLog.append("Is " + ((Squishee) retVal).getName() + " Good Enough? \n");
+        AopLog.append("Is " + ((Squishee) retVal).getName() + " Good Enough?%n");
     }
 
+    @AfterThrowing("execution(* sellSquishee(..))")
     public void sayYouAreNotAllowed() {
-        AopLog.append("Hmmm... \n");
+        AopLog.append("Hmmm...%n");
     }
 
+    @After("execution(* sellSquishee(..))")
     public void sayGoodBye() {
-        AopLog.append("Good Bye! \n");
+        AopLog.append("Good Bye!%n");
     }
 
+    @Around("execution(* sellSquishee(..))")
     public Object sayPoliteWordsAndSell(ProceedingJoinPoint pjp) throws Throwable {
-        AopLog.append("Hi! \n");
+        AopLog.append("Hi!%n");
         Object retVal = pjp.proceed();
-        AopLog.append("See you! \n");
+        AopLog.append("See you!%n");
         return retVal;
     }
-
 }
--- src/test/java/aop/AopAspectJExceptionTest.java
+++ src/test/java/aop/AopAspectJExceptionTest.java
@@ -4,10 +4,10 @@ import lab.aop.AopLog;
 import lab.aop.Bar;
 import lab.aop.CustomerBrokenException;
 import lab.model.Person;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,16 +22,11 @@ class AopAspectJExceptionTest {
 	private Bar bar;
     
 	@Autowired
+    @Qualifier("broken")
     private Person person;
-
-    @BeforeEach
-    void setUp() throws Exception {
-//        person.setBroke(true);
-    }
 
     @Test
     void testAfterThrowingAdvice() {
- 
     	assertThrows(CustomerBrokenException.class, () -> bar.sellSquishee(person));
 
         assertTrue("Customer is not broken ", AopLog.getStringValue().contains("Hmmm..."));
