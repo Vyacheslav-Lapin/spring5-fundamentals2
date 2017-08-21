5662d941af4033afe3412ae3e8fa29a2469a5f8e
ilia gusevskii <ilia_gusevskii@epam.com>
2017-08-21 17:49:16 +0300

    lab 6 done

--- src/test/java/jdbc/JdbcTest.java
+++ src/test/java/jdbc/JdbcTest.java
@@ -15,8 +15,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:jdbc.xml")
@@ -27,7 +28,7 @@ class JdbcTest {
 	
     private List<Country> expectedCountryList = new ArrayList<Country>();
     private List<Country> expectedCountryListStartsWithA = new ArrayList<Country>();
-    private Country countryWithChangedName = new SimpleCountry(1, "Russia", "RU");
+    private Country countryWithChangedName = new SimpleCountry(8, "Russia", "RU");
 
     @BeforeEach
     void setUp() throws Exception {
@@ -36,25 +37,17 @@ class JdbcTest {
     }
     
     @Test
-    @DirtiesContext
     void testCountryList() {
         List<Country> countryList = countryDao.getCountryList();
-        assertNotNull(countryList);
-        assertEquals(expectedCountryList.size(), countryList.size());
-        for (int i = 0; i < expectedCountryList.size(); i++) {
-            assertEquals(expectedCountryList.get(i), countryList.get(i));
-        }
+
+        assertThat(expectedCountryList, is(countryList));
     }
 
     @Test
-    @DirtiesContext
     void testCountryListStartsWithA() {
         List<Country> countryList = countryDao.getCountryListStartWith("A");
-        assertNotNull(countryList);
-        assertEquals(expectedCountryListStartsWithA.size(), countryList.size());
-        for (int i = 0; i < expectedCountryListStartsWithA.size(); i++) {
-            assertEquals(expectedCountryListStartsWithA.get(i), countryList.get(i));
-        }
+
+        assertThat(expectedCountryListStartsWithA, is(countryList));
     }
 
     @Test
@@ -65,9 +58,9 @@ class JdbcTest {
     }
 
     private void initExpectedCountryLists() {
-         for (int i = 0; i < SimpleCountryJdbcDao.COUNTRY_INIT_DATA.length; i++) {
+         for (int i = 0; i < SimpleCountryJdbcDao.COUNTRY_INIT_DATA.length;) {
              String[] countryInitData = SimpleCountryJdbcDao.COUNTRY_INIT_DATA[i];
-             Country country = new SimpleCountry(i, countryInitData[0], countryInitData[1]);
+             Country country = new SimpleCountry(++i, countryInitData[0], countryInitData[1]);
              expectedCountryList.add(country);
              if (country.getName().startsWith("A")) {
                  expectedCountryListStartsWithA.add(country);
