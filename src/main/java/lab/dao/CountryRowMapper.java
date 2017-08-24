3715782e802e611c906c1f4dbb5e09a078e1dba6
eugen <evgenii_aleksandrov@epam.com>
2017-08-24 15:02:44 +0300

    Lab-6 is done.

--- src/main/java/lab/dao/CountryRowMapper.java
+++ src/main/java/lab/dao/CountryRowMapper.java
@@ -8,16 +8,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CountryRowMapper implements RowMapper<Country> {
-	public static final String ID = "id";
-	public static final String NAME = "name";
-	public static final String CODE_NAME = "code_name";
+    public static final String ID = "id";
+    public static final String NAME = "name";
+    public static final String CODE_NAME = "code_name";
 
-	public Country mapRow(ResultSet resultSet, int i) throws SQLException {
-		Country country = new SimpleCountry(resultSet.getInt(ID), resultSet.getString(NAME),
-				// TODO: implement it
-				null
-				);
-		
-		return country;
-	}
+    public Country mapRow(ResultSet resultSet, int i) throws SQLException {
+        Country country = new SimpleCountry(resultSet.getInt(ID), resultSet.getString(NAME),
+                resultSet.getString(CODE_NAME)
+        );
+
+        return country;
+    }
 }
--- src/test/java/jdbc/JdbcTest.java
+++ src/test/java/jdbc/JdbcTest.java
@@ -25,9 +25,9 @@ class JdbcTest {
 	@Autowired
 	private CountryDao countryDao;
 	
-    private List<Country> expectedCountryList = new ArrayList<Country>();
-    private List<Country> expectedCountryListStartsWithA = new ArrayList<Country>();
-    private Country countryWithChangedName = new SimpleCountry(1, "Russia", "RU");
+    private List<Country> expectedCountryList = new ArrayList<>();
+    private List<Country> expectedCountryListStartsWithA = new ArrayList<>();
+    private Country countryWithChangedName = new SimpleCountry(8, "Russia", "RU");
 
     @BeforeEach
     void setUp() throws Exception {
@@ -65,9 +65,9 @@ class JdbcTest {
     }
 
     private void initExpectedCountryLists() {
-         for (int i = 0; i < SimpleCountryJdbcDao.COUNTRY_INIT_DATA.length; i++) {
+         for (int i = 0; i < SimpleCountryJdbcDao.COUNTRY_INIT_DATA.length;) {
              String[] countryInitData = SimpleCountryJdbcDao.COUNTRY_INIT_DATA[i];
-             Country country = new SimpleCountry(i, countryInitData[0], countryInitData[1]);
+             Country country = new SimpleCountry(++i, countryInitData[0], countryInitData[1]);
              expectedCountryList.add(country);
              if (country.getName().startsWith("A")) {
                  expectedCountryListStartsWithA.add(country);
